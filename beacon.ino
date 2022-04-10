e2e2dde3aed26ad32c246efd9471c68de36bb085
Nat Atnafu <natnafu@gmail.com>
2022-04-10 17:40:00 -0400

    Rip out compass and gps code

--- beacon.ino
+++ beacon.ino
@@ -1,20 +1,21 @@
-#include "compass.h"
-#include "gps.h"
 #include "pixels.h"
 #include "radio.h"
 
+#define PIN_LED 13
+
+uint8_t radio_tx_self();
+uint8_t radio_rx_other();
+
 typedef struct {
   uint8_t ID;
-  float gps_lat;
-  float gps_lng;
 } Beacon;
 
 #define SELF  0
 #define OTHER 1
 
 Beacon beacons[2] = {
-  [SELF]  = {SELF , 0, 0},
-  [OTHER] = {OTHER, 0, 0},
+  [SELF]  = {SELF},
+  [OTHER] = {OTHER},
 };
 
 // Timer for sending a LORA transmission every TX_FREQ_MS
@@ -32,8 +33,8 @@ void setup(void)
 
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
 
   pixels_init();
 
@@ -41,80 +42,22 @@ void setup(void)
   tx_timer = millis();
   Serial.println("radio_init success!");
 
-  gps_init();
-  Serial.println("gps_init success!");
-
-  // Runs calibration on each boot 
-  compass_init();
-  Serial.println("compass_init success!");
-
   pixels_set_all(0,0,0);
 }
-
-double Heading;
-double path;
 
 void loop()
 {  
   // Status blinky
-  digitalWrite(13, !digitalRead(13));
-  
-  // Get compass heading
-  Heading = compass_get_heading();
-  Serial.print("Heading: "); Serial.println(Heading);
+  // digitalWrite(PIN_LED, !digitalRead(PIN_LED));
 
-  // Update GPS
-  gps_update(); // needs to be called every loop
-  beacons[SELF].gps_lat = gps_get_lat();
-  beacons[SELF].gps_lng = gps_get_lng();
 
-  if ((millis() - tx_timer > TX_FREQ_MS) &&
-        beacons[SELF].gps_lat &&
-        beacons[SELF].gps_lng) {
-    // Transmit every 5s if lat and long are valid
+  if ((millis() - tx_timer > TX_FREQ_MS)) {
+    digitalWrite(PIN_LED, !digitalRead(PIN_LED));
     radio_tx_self();
-
-    radio_rx_other();
-    
+    radio_rx_other();    
     tx_timer = millis();
   }
-
-  // calculate path to other beacon
-  if (beacons[SELF].gps_lat == 0 || beacons[SELF].gps_lng == 0) {
-    pixels_set_all(0,0,5);  // SELF location not valid
-  } else if (beacons[OTHER].gps_lat == 0 || beacons[OTHER].gps_lng == 0) {
-    pixels_set_all(0,5,0);  // OTHER location not valid
-  } else {
-    double courseToOther =
-          TinyGPSPlus::courseTo(
-            beacons[SELF].gps_lat,
-            beacons[SELF].gps_lng,
-            beacons[OTHER].gps_lat, 
-            beacons[OTHER].gps_lng);
-    
-    // calculate which LED points towards other beacon
-    path = courseToOther - Heading;
-    if (path < 0.0) path += 360.0;
-    if (path > 360.0) path -= 360.0;
-  
-    pixels_set_all(0,0,0);
-    pixels_set_one((uint32_t(path*24.0/360.0)), 25,0,0);
-  }
-
-  // Status message
-  if (millis() - st_timer > STATUS_FREQ_MS) {
-    Serial.println("-GPS-");
-    Serial.print("SELF: "); Serial.print(beacons[SELF].gps_lat,6);
-    Serial.print(","); Serial.println(beacons[SELF].gps_lng,6);
-    Serial.print("OTHER: "); Serial.print(beacons[OTHER].gps_lat,6);
-    Serial.print(","); Serial.println(beacons[OTHER].gps_lng,6);
-    Serial.println("-COMPASS-");
-    Serial.print("Heading: "); Serial.println(Heading);
-    Serial.print("Path: "); Serial.println(path);
-    st_timer = millis();
-  }
 }
-
 /////////////////////////// RADIO STUFF /////////////////////////////////////
 
 // Sends data over LoRa
@@ -141,8 +84,6 @@ uint8_t radio_rx_other() {
     {
       temp = (Beacon *)buf;
       beacons[OTHER].ID = temp->ID;
-      beacons[OTHER].gps_lat = temp->gps_lat;
-      beacons[OTHER].gps_lng = temp->gps_lng;
       Serial.println("RX success!");
       return 0;
     }
